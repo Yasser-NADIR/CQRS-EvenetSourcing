9b145ba07f85f005004b1d068758b6832ebfd021
yasser nadir <yassernadir761@gmail.com>
2022-08-23 10:59:03 +0100

    creating command objects

--- bank-account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommandApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(CommandApplication.class, args);
 	}
 
 }
