909aea1c825ec53f3010c2d9f540a05a2fd70004
yasser nadir <yassernadir761@gmail.com>
2022-08-23 09:58:51 +0100

    setup project

--- bank-account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -1,10 +1,10 @@
-package com.techbank.cqrs.core;
+package com.techbank.account.cmd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class CommandApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- bank-account/account.query/src/main/java/com/techbank/account/query/QueryApplication.java
+++ bank-account/account.query/src/main/java/com/techbank/account/query/QueryApplication.java
@@ -1,10 +1,10 @@
-package com.techbank.account.common;
+package com.techbank.account.query;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class QueryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
