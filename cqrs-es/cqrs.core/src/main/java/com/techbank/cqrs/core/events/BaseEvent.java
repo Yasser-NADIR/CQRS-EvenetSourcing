497466490613da54c73287007f463a1d81b62d1f
yasser nadir <yassernadir761@gmail.com>
2022-08-24 18:00:48 +0100

    correction abount superbuilder

--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/events/BaseEvent.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/events/BaseEvent.java
@@ -5,8 +5,9 @@ import com.techbank.cqrs.core.messages.Message;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-@Data @AllArgsConstructor @NoArgsConstructor //@SuperBuilder
+@Data @AllArgsConstructor @NoArgsConstructor @SuperBuilder
 public abstract class BaseEvent extends Message{
     private int version;
 }
